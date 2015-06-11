b666517594cd6740b69dbd5e03033f19de98e934
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-06-11 12:53:16 +0000

    js 修改

--- fanqie-toms/trunk/src/main/webapp/assets/js/KeZhanHuoYueBiao.js
+++ fanqie-toms/trunk/src/main/webapp/assets/js/KeZhanHuoYueBiao.js
@@ -1,7 +1,7 @@
 $(function(){
 	// 设置左边三列的背景颜色
 	for(var i=1; i<$('.table tr').length; i++){
-		$('.table tr').eq(i).children('td').slice(0,3).css('background','#e5f1f4');
+		$('.table tr').eq(i).children('td').slice(0,2).css('background','#e5f1f4');
 	}
 
 })
