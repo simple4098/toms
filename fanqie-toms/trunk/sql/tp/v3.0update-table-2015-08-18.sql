7cb88ae837f72169a9a73253dcb01a7b8024e295
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-08-24 10:09:49 +0000

    v2.0sql

--- fanqie-toms/trunk/sql/tp/v3.0update-table-2015-08-18.sql
+++ fanqie-toms/trunk/sql/tp/v3.0update-table-2015-08-18.sql
@@ -4,3 +4,4 @@ ALTER TABLE ota_info DROP COLUMN "company_id", DROP COLUMN "appkey", DROP COLUMN
 alter table ota_inn_ota add column bang_inn_id varchar(64);
 alter table ota_inn_ota add column ota_info_id varchar(64);
 alter table ota_inn_ota add column inn_id int8;
+alter table ota_inn_ota add column sj int2;
