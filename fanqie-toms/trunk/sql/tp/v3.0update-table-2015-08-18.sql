653993fe4933cda9e3072c41b1c73d640d111eba
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-08-25 01:40:24 +0000

    优化， 上测试环境配置

--- fanqie-toms/trunk/sql/tp/v3.0update-table-2015-08-18.sql
+++ fanqie-toms/trunk/sql/tp/v3.0update-table-2015-08-18.sql
@@ -1,7 +1,11 @@
 ALTER TABLE ota_info DROP COLUMN "company_id", DROP COLUMN "appkey", DROP COLUMN "appsecret",DROP COLUMN "sessionkey",DROP COLUMN "expired_time";
+alter table ota_info add column sort int2;
 
 
 alter table ota_inn_ota add column bang_inn_id varchar(64);
 alter table ota_inn_ota add column ota_info_id varchar(64);
 alter table ota_inn_ota add column inn_id int8;
 alter table ota_inn_ota add column sj int2;
+
+ALTER TABLE ota_toms_order ADD COLUMN company_id varchar(64);
+COMMENT ON COLUMN "public"."ota_toms_order"."company_id" IS '公司id';
--- fanqie-toms/trunk/src/main/java/com/fanqielaile/toms/controller/SystemController.java
+++ fanqie-toms/trunk/src/main/java/com/fanqielaile/toms/controller/SystemController.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -293,7 +294,7 @@ public class SystemController extends BaseController {
      * @param permission
      * @return
      */
-    @RequestMapping("create_permission")
+    @RequestMapping(value = "create_permission",method = RequestMethod.POST)
     public String createPermission(Permission permission) {
         permissionService.createPermission(permission);
         return "permission";
