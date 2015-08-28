a7c8c3f49d4c72aad1045b8e58697c903c272a25
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-08-28 06:09:50 +0000

    发布前奏

--- fanqie-toms/trunk/sql/tp/v3.0update-table-2015-08-18.sql
+++ fanqie-toms/trunk/sql/tp/v3.0update-table-2015-08-18.sql
@@ -6,6 +6,8 @@ alter table ota_inn_ota add column bang_inn_id varchar(64);
 alter table ota_inn_ota add column ota_info_id varchar(64);
 alter table ota_inn_ota add column inn_id int8;
 alter table ota_inn_ota add column sj int2;
+--创建索引
+CREATE INDEX  ON "public"."ota_inn_ota" ("wg_hid", "company_id", "bang_inn_id", "ota_info_id");
 
 ALTER TABLE ota_toms_order ADD COLUMN company_id varchar(64);
 COMMENT ON COLUMN "public"."ota_toms_order"."company_id" IS '公司id';
