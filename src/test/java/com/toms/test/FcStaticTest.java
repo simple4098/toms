9a22d33a4dbbef5b2b0e7781cb8f716fc59bb466
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-09-06 02:19:37 +0000

    test

--- src/test/java/com/toms/test/FcStaticTest.java
+++ src/test/java/com/toms/test/FcStaticTest.java
@@ -8,7 +8,11 @@ import com.fanqielaile.toms.dao.IFcProvinceDao;
 import com.fanqielaile.toms.model.fc.FcArea;
 import com.fanqielaile.toms.model.fc.*;
 import com.fanqielaile.toms.support.tb.TBXHotelUtil;
+import com.fanqielaile.toms.support.util.FcUtil;
 import com.fanqielaile.toms.support.util.XmlDeal;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.Dom4JDriver;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +25,8 @@ import javax.annotation.Resource;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,11 +63,11 @@ public class FcStaticTest {
             fcProvinceDao.saveFcProvince(fcProvince);
         }*/
 
-        String xml = HttpClientUtil.httpGets(country, null);
+        /*String xml = HttpClientUtil.httpGets(country, null);
         List<FcArea> list = XmlDeal.pareFcArea(xml);
         for (FcArea fcArea:list){
             fcAreaDao.saveFcArea(fcArea);
-        }
+        }*/
     }
     @Test
     public void beanToXML() throws JAXBException {
@@ -73,7 +79,7 @@ public class FcStaticTest {
         hotelInfoList.add(hotelInfo);
         AddHotelMappingRequest addHotelMappingRequest = new AddHotelMappingRequest();
         addHotelMappingRequest.setHotelList(hotelInfoList);
-        Header header = new Header(RequestType.addHotelMapping,"test","1111");
+        Header header = new Header(RequestType.addHotelMapping,"S10085349","security_test_S10085349");
         AddHotelRequest addHotelRequest = new AddHotelRequest(header,addHotelMappingRequest);
         try {
             JAXBContext context = JAXBContext.newInstance(AddHotelRequest.class);
@@ -82,7 +88,37 @@ public class FcStaticTest {
         } catch (JAXBException e) {
             e.printStackTrace();
         }
+    }
 
+    @Test
+    public void GetHotelRequest() throws JAXBException {
+        GetHotelInfoRequest getHotelInfoRequest = new GetHotelInfoRequest();
+        getHotelInfoRequest.setFcHotelIds("112324");
+        Header header = new Header(RequestType.addHotelMapping,"S10085349","security_test_S10085349");
+        GetHotelRequest getHotelRequest = new GetHotelRequest(header,getHotelInfoRequest);
+
+        /*XStream xStream = new XStream(new DomDriver("utf-8"));
+        xStream.processAnnotations(GetHotelRequest.class);
+        System.out.println(xStream.toXML(getHotelInfoRequest));*/
+        try {
+           /* JAXBContext context = JAXBContext.newInstance(GetHotelRequest.class);
+            Marshaller marshaller = context.createMarshaller();
+            StringWriter fw = new StringWriter();
+            marshaller.marshal(getHotelRequest,fw);
+            String string = fw.toString();*/
+
+            String s = FcUtil.fcRequest(getHotelRequest);
+            System.out.println(s);
+
+           /* try {
+                String gets = HttpClientUtil.httpPost("http://www.fangcang.org/USP/api_v1/getHotelInfo", string);
+                System.out.println(gets);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }*/
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 
 }
