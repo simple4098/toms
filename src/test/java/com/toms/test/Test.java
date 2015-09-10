97fda5da485717ac11d6bc1d99053b24c7ede3a7
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-09-10 06:40:28 +0000

    svn sumbit  测试

--- src/test/java/com/toms/test/Test.java
+++ src/test/java/com/toms/test/Test.java
@@ -3,8 +3,11 @@ package com.toms.test;
 
 import com.fanqie.util.DcUtil;
 import com.fanqielaile.toms.dto.PushRoom;
+import com.fanqielaile.toms.support.util.TomsUtil;
 import com.fanqielaile.toms.support.util.XmlDeal;
+import org.apache.commons.lang.ArrayUtils;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -25,11 +28,17 @@ public class Test {
 //        long after = System.currentTimeMillis();
 //        System.out.println("DOM用时"+(after-begin)+"毫秒");
 
-        String[] weekDaysName = {"日", "一", "二", "三", "四", "五", "六"};
+        /*String[] weekDaysName = {"日", "一", "二", "三", "四", "五", "六"};
         String[] weekDaysCode = {"0", "1", "2", "3", "4", "5", "6"};
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
         int intWeek = calendar.get(Calendar.DAY_OF_WEEK) - 1;
-        System.out.println(DcUtil.dayOfWeek(intWeek));
+        System.out.println(DcUtil.dayOfWeek(intWeek));*/
+        List<String> dateList = new ArrayList<String>();
+        dateList.add("2015-05-06");
+        dateList.add("2015-06-06");
+        dateList.add("2015-07-06");
+        String string = TomsUtil.listDateToStr(dateList);
+        System.out.println(string);
     }
 }
