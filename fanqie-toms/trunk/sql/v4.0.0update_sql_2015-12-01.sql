77033739fb76a58cb609af8f736883f5644f2094
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-12-07 02:23:47 +0000

    线上 sql脚本

--- fanqie-toms/trunk/sql/v4.0.0update_sql_2015-12-01.sql
+++ fanqie-toms/trunk/sql/v4.0.0update_sql_2015-12-01.sql
@@ -21,3 +21,6 @@ CREATE INDEX "bang_inn_company_id_inn_id_idx" ON "public"."bang_inn" USING btree
 DROP INDEX "public"."ota_inn_ota_wg_hid_company_id_bang_inn_id_ota_info_id_idx";
 CREATE INDEX "ota_inn_ota_wg_hid_company_id_bang_inn_id_ota_info_id_idx" ON "public"."ota_inn_ota" USING btree ("wg_hid" "pg_catalog"."text_ops", "company_id" "pg_catalog"."text_ops", "bang_inn_id" "pg_catalog"."text_ops", "ota_info_id" "pg_catalog"."text_ops", "sj");
 
+UPDATE  otainfo_company_ref set create_date='2015-07-01 00:00:00' where id='1';
+UPDATE  otainfo_company_ref set create_date='2015-10-01 00:00:00' where id='572c1b47-9343-4567-981c-895ki8j18';
+
