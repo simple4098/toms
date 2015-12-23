5ccd654a58a04d9cff1e4eaaeba4687976019a5c
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-12-23 11:47:34 +0000

    读取excel数据

--- src/main/java/com/fanqielaile/toms/dao/CtripHotelRoomTypeDao.java
+++ src/main/java/com/fanqielaile/toms/dao/CtripHotelRoomTypeDao.java
@@ -13,5 +13,17 @@ import com.fanqielaile.toms.dto.ctrip.CtripHotelRoomType;
 public interface CtripHotelRoomTypeDao {
 	
 	public  List<CtripHotelRoomType> findByCtripParentHotelId(@Param("ctripParentHotelId") String ctripParentHotelId);
-  
+
+	/**
+	 * 保存携程母酒店对应的母房型
+	 * @param ctripHotelRoomType
+	 */
+	void saveCtripHotelRoomType(CtripHotelRoomType ctripHotelRoomType);
+
+	/**
+	 *
+	 * @param parentHotelId 母酒店id
+	 * @param roomTypeId 母房型id
+	 */
+	CtripHotelRoomType findByCtripParentHotelIdAndRoomTypeId(@Param("parentHotelId")String parentHotelId, @Param("roomTypeId")String roomTypeId);
 }
