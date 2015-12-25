bb5a1e1b9cbb4581b343bebadae35d71e9cfc109
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-12-25 03:13:36 +0000

    响应xml转化成 RequestResponse

--- fanqie-toms/trunk/src/main/java/com/fanqielaile/toms/support/util/FcUtil.java
+++ fanqie-toms/trunk/src/main/java/com/fanqielaile/toms/support/util/FcUtil.java
@@ -3,6 +3,7 @@ package com.fanqielaile.toms.support.util;
 
 import com.fanqie.bean.response.RequestResponse;
 import com.fanqie.support.OtaRequest;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -13,34 +14,34 @@ import java.io.StringWriter;
 
 /**
  * DESC : 房仓对象转化成xml字符串
+ *
  * @author : 番茄木-ZLin
  * @data : 2015/9/6
  * @version: v1.0.0
  */
-public  class FcUtil<T extends OtaRequest> {
+public class FcUtil<T extends OtaRequest> {
 
-    public static   <T>  String  fcRequest(T t) throws JAXBException {
+    public static <T> String fcRequest(T t) throws JAXBException {
         JAXBContext context = JAXBContext.newInstance(t.getClass());
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);// 是否省略xm头声明信
 //        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,Boolean.TRUE);
         StringWriter fw = new StringWriter();
-        marshaller.marshal(t,fw);
-        return  fw.toString();
+        marshaller.marshal(t, fw);
+        return fw.toString();
     }
 
     /**
      * 响应xml转化成 RequestResponse
+     *
      * @param xml 响应xml
      */
-    public static RequestResponse xMLStringToBean(String xml){
-        try {
+    public static RequestResponse xMLStringToBean(String xml) throws JAXBException {
+        if (!StringUtils.isEmpty(xml)) {
             JAXBContext context = JAXBContext.newInstance(RequestResponse.class);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            RequestResponse response = (RequestResponse)unmarshaller.unmarshal(new StringReader(xml));
-            return  response;
-        } catch (JAXBException e) {
-            e.printStackTrace();
+            RequestResponse response = (RequestResponse) unmarshaller.unmarshal(new StringReader(xml));
+            return response;
         }
         return null;
     }
