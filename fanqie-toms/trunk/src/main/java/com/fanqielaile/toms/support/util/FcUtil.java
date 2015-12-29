e3bea0b8b6d451f1a685bf23b8507604af183796
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-12-29 07:22:04 +0000

    xml To Bean 修改

--- fanqie-toms/trunk/src/main/java/com/fanqielaile/toms/support/util/FcUtil.java
+++ fanqie-toms/trunk/src/main/java/com/fanqielaile/toms/support/util/FcUtil.java
@@ -2,14 +2,16 @@ package com.fanqielaile.toms.support.util;
 
 
 import com.fanqie.bean.response.RequestResponse;
+import com.fanqie.bean.response.RequestResult;
 import com.fanqie.support.OtaRequest;
 import org.apache.commons.lang3.StringUtils;
+import org.dom4j.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import java.io.StringReader;
 import java.io.StringWriter;
 
 /**
@@ -21,6 +23,7 @@ import java.io.StringWriter;
  */
 public class FcUtil<T extends OtaRequest> {
 
+    private static final Logger log = LoggerFactory.getLogger(FcUtil.class);
     public static <T> String fcRequest(T t) throws JAXBException {
         JAXBContext context = JAXBContext.newInstance(t.getClass());
         Marshaller marshaller = context.createMarshaller();
@@ -37,10 +40,23 @@ public class FcUtil<T extends OtaRequest> {
      */
     public static RequestResponse xMLStringToBean(String xml) throws JAXBException {
         if (!StringUtils.isEmpty(xml)) {
-            JAXBContext context = JAXBContext.newInstance(RequestResponse.class);
+            try {
+                RequestResponse response1 = new RequestResponse();
+                Element element = XmlDeal.dealXmlStr(xml);
+                String message = element.element("RequestResult").element("Message").getText();
+                String resultCode = element.element("RequestResult").element("ResultCode").getText();
+                RequestResult requestResult = new RequestResult();
+                requestResult.setMessage(message);
+                requestResult.setResultCode(resultCode!=null?Integer.valueOf(resultCode):-1);
+                response1.setRequestResult(requestResult);
+                return response1;
+            } catch (Exception e) {
+                log.error("解析xml异常:",e);
+            }
+            /*JAXBContext context = JAXBContext.newInstance(RequestResponse.class);
             Unmarshaller unmarshaller = context.createUnmarshaller();
             RequestResponse response = (RequestResponse) unmarshaller.unmarshal(new StringReader(xml));
-            return response;
+            return response;*/
         }
         return null;
     }
