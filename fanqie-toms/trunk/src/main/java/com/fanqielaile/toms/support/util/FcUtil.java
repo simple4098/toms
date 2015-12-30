36ca4620031035104fc108361147e60de7133df4
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-12-30 07:38:02 +0000

    携程：xml  to bean 异常

--- fanqie-toms/trunk/src/main/java/com/fanqielaile/toms/support/util/FcUtil.java
+++ fanqie-toms/trunk/src/main/java/com/fanqielaile/toms/support/util/FcUtil.java
@@ -1,8 +1,7 @@
 package com.fanqielaile.toms.support.util;
 
 
-import com.fanqie.bean.response.RequestResponse;
-import com.fanqie.bean.response.RequestResult;
+import com.fanqie.bean.response.*;
 import com.fanqie.support.OtaRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.dom4j.Element;
@@ -15,6 +14,8 @@ import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * DESC : 房仓对象转化成xml字符串
@@ -42,11 +43,46 @@ public class FcUtil<T extends OtaRequest> {
      */
     public static RequestResponse xMLStringToBean(String xml) throws JAXBException {
         if (!StringUtils.isEmpty(xml)) {
-            JAXBContext context = JAXBContext.newInstance(RequestResponse.class);
+                try {
+                    RequestResponse response1 = new RequestResponse();
+                    Element element = XmlDeal.dealXmlStr(xml);
+                    String message = element.element("RequestResult").element("Message").getText();
+                    String resultCode = element.element("RequestResult").element("ResultCode").getText();
+                    List<Element> elements = element.element("RequestResult").element("Response")
+                            .element("HotelGroupInterfaceRoomTypeListResponse")
+                            .element("HotelGroupInterfaceRoomTypeList").elements("HotelGroupInterfaceRoomTypeEntity");
+                    Response response = new Response();
+                    if (elements!=null){
+                        HotelGroupInterfaceRoomTypeListResponse roomTypeListResponse = new HotelGroupInterfaceRoomTypeListResponse();
+                        HotelGroupInterfaceRoomTypeEntity entity = null;
+                        List<HotelGroupInterfaceRoomTypeEntity> HotelGroupInterfaceRoomTypeList = new ArrayList<>();
+                        for (Element e:elements){
+                            entity = new HotelGroupInterfaceRoomTypeEntity();
+                            entity.setHotelGroupRoomTypeCode(e.element("HotelGroupRoomTypeCode").getText());;
+                            entity.setHotel(e.element("Hotel").getText());
+                            entity.setRoom(e.element("Room").getText());
+                            HotelGroupInterfaceRoomTypeList.add(entity);
+                        }
+                        roomTypeListResponse.setHotelGroupInterfaceRoomTypeList(HotelGroupInterfaceRoomTypeList);
+                        response.setHotelGroupInterfaceRoomTypeListResponse(roomTypeListResponse);
+                    }else {
+                        log.info("xml 异常");
+                    }
+                    RequestResult requestResult = new RequestResult();
+                    requestResult.setMessage(message);
+                    requestResult.setResultCode(resultCode!=null?Integer.valueOf(resultCode):-1);
+                    requestResult.setResponse(response);
+                    response1.setRequestResult(requestResult);
+                    return response1;
+                } catch (Exception e) {
+                    log.error("解析xml异常:",e);
+                }
+
+        }
+            /*JAXBContext context = JAXBContext.newInstance(RequestResponse.class);
             Unmarshaller unmarshaller = context.createUnmarshaller();
             RequestResponse response = (RequestResponse) unmarshaller.unmarshal(new StringReader(xml));
-            return response;
-        }
+            return response;*/
         return null;
     }
 }
