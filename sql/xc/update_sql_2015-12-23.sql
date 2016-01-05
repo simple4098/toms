4d39c41720e30f120dce34f3abdf772efc6c72b3
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2016-01-05 08:05:55 +0000

    携程：设置房价 房态  bug， 初始化sql

--- sql/xc/update_sql_2015-12-23.sql
+++ sql/xc/update_sql_2015-12-23.sql
@@ -16,43 +16,16 @@ COMMENT ON COLUMN "public"."ota_order_guests"."first_name" IS '英文名';
 
 COMMENT ON COLUMN "public"."ota_order_guests"."last_name" IS '英文姓';
 
--- 新增携程基础酒店信息表
--- ----------------------------
--- Table structure for ctrip_hotel_info
--- ----------------------------
-DROP TABLE IF EXISTS "public"."ctrip_hotel_info";
-CREATE TABLE "public"."ctrip_hotel_info" (
-"id" varchar(64) COLLATE "default" NOT NULL,
-"hotel_id" varchar(30) COLLATE "default",
-"hotel_name" varchar(100) COLLATE "default",
-"country_name" varchar(100) COLLATE "default",
-"city_name" varchar(100) COLLATE "default",
-"address" varchar(100) COLLATE "default",
-"telephone" varchar(20) COLLATE "default",
-"created_date" timestamp(6),
-"updated_date" timestamp(6),
-"deleted" int2
-)
-WITH (OIDS=FALSE)
+ALTER TABLE fc_rate_plan RENAME TO ota_rate_plan;
 
-;
-COMMENT ON COLUMN "public"."ctrip_hotel_info"."id" IS 'ID';
-COMMENT ON COLUMN "public"."ctrip_hotel_info"."hotel_id" IS '携程酒店id';
-COMMENT ON COLUMN "public"."ctrip_hotel_info"."hotel_name" IS '携程酒店名称';
-COMMENT ON COLUMN "public"."ctrip_hotel_info"."country_name" IS '酒店所在国家名称';
-COMMENT ON COLUMN "public"."ctrip_hotel_info"."city_name" IS '酒店所在城市名称';
-COMMENT ON COLUMN "public"."ctrip_hotel_info"."address" IS '详细地址';
-COMMENT ON COLUMN "public"."ctrip_hotel_info"."telephone" IS '联系电话';
-COMMENT ON COLUMN "public"."ctrip_hotel_info"."created_date" IS '创建时间';
-COMMENT ON COLUMN "public"."ctrip_hotel_info"."updated_date" IS '更新时间';
-COMMENT ON COLUMN "public"."ctrip_hotel_info"."deleted" IS '是否删除';
+ALTER TABLE "public"."ota_rate_plan" ADD COLUMN "rate_code_default" bool;
+ALTER TABLE "public"."ota_rate_plan" ADD COLUMN "rate_plan_code" varchar(50);
 
--- ----------------------------
--- Alter Sequences Owned By
--- ----------------------------
 
--- ----------------------------
--- Primary Key structure for table ctrip_hotel_info
--- ----------------------------
-ALTER TABLE "public"."ctrip_hotel_info" ADD PRIMARY KEY ("id");
+ALTER TABLE "public"."otainfo_company_ref" ADD COLUMN "xc_user_name" varchar(50);
+ALTER TABLE "public"."otainfo_company_ref" ADD COLUMN "xc_password" varchar(50);
+ALTER TABLE "public"."otainfo_company_ref" ADD COLUMN "user_id" varchar(50);
+ALTER TABLE "public"."otainfo_company_ref" ADD COLUMN "updated_date" timestamp(6);
+
+INSERT INTO  ota_info(id,ota_info,ota_type,deleted,sort)  VALUES ('3', '携程', 'XC', 0, 3);
 
