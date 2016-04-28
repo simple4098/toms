4be333240f5095b774882dcd9424b4467724cc85
xiongkejian <xiongkejian@671eefe9-2466-459f-b5fc-367f35298b91>
2016-04-28 01:19:17 +0000

    查询订单功能新增其他消费相关

--- fanqie-toms/trunk/sql/v4.9.0update_sql_2016-04-15.sql
+++ fanqie-toms/trunk/sql/v4.9.0update_sql_2016-04-15.sql
@@ -13,3 +13,8 @@ ADD COLUMN "xml_data" varchar(255);
 COMMENT ON COLUMN "public"."ota_toms_order"."xml_data" IS '创建订单请求的xml数据';
 ALTER TABLE "public"."ota_toms_order"
 ALTER COLUMN "xml_data" TYPE text COLLATE "default";
+
+ALTER TABLE "public"."ota_toms_order"
+ADD COLUMN "user_id" varchar(64);
+
+COMMENT ON COLUMN "public"."ota_toms_order"."user_id" IS '操作人id';
