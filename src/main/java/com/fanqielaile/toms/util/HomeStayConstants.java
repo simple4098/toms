5ad52bd5fb2639d25b6d265bef5b064e4145dd15
lizuoqing <lizuoqing@671eefe9-2466-459f-b5fc-367f35298b91>
2016-09-26 06:52:22 +0000

    民宿修改

--- src/main/java/com/fanqielaile/toms/util/HomeStayConstants.java
+++ src/main/java/com/fanqielaile/toms/util/HomeStayConstants.java
@@ -3,17 +3,18 @@ package com.fanqielaile.toms.util;
 public interface HomeStayConstants {
 	int minBookingDays = 1;
 	int maxBookingDays = 999;
-	String checkOutTime = "14:00";
-	String checkInTime = "23:00";
-	String receptionHours = "23:00";
+	String checkOutTime = "12:00";
+	String checkInTime = "14:00";
+	String receptionHours = "14:00 - 23：00";
 	int depositAmount = 0;
 	String houseModel = "标准间";
 	String avatarUrl ="http://img.fanqiele.com/imgFtpFolder/1474337511842/yflx.jpg";
-	String depositType = "Online";
+	String depositType = "1";
 	
 	int invoiceType = 0;
 	int hasLandlord = 0;
 	int refundDays = 1;
+	String tradingRules = "入住前1天 14:00如果取消订单，预付定金将全部退还";
 	int instantBook = 1;
 	/**
 	 * 线上支付比例
